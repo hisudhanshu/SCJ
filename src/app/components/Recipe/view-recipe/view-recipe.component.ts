1351113d24cb2a61394366cc27e47efdeb5a6968
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-07-06 17:45:09 +0530

    View Recipe Screen Change

--- src/app/components/Recipe/view-recipe/view-recipe.component.ts
+++ src/app/components/Recipe/view-recipe/view-recipe.component.ts
@@ -7,43 +7,7 @@ import { AuthServicesService } from 'src/app/Service/auth-services.service';
   styleUrls: ['./view-recipe.component.css']
 })
 export class ViewRecipeComponent implements OnInit {
-  selectedProduct: string | null = null;
-  recipes: Recipe[] = [];
 
-  constructor(private authService: AuthServicesService) {}
-
-  ngOnInit(): void {}
-
-  selectProduct(event: Event) {
-    const target = event.target as HTMLSelectElement;
-    const product = target.value;
-    this.selectedProduct = product;
-    this.loadRecipes();
-  }
-
-  loadRecipes() {
-    if (this.selectedProduct === 'Bottle') {
-      this.recipes = [
-        { name: 'Recipe 1', ingredients: 'Ingredient 1, Ingredient 2' },
-        { name: 'Recipe 2', ingredients: 'Ingredient 3, Ingredient 4' }
-      ];
-    } else if (this.selectedProduct === 'Glass') {
-      this.recipes = [
-        { name: 'Recipe 1', ingredients: 'Ingredient 5, Ingredient 6' },
-        { name: 'Recipe 2', ingredients: 'Ingredient 7, Ingredient 8' }
-      ];
-    } else if (this.selectedProduct === 'Household') {
-      this.recipes = [
-        { name: 'Recipe 1', ingredients: 'Ingredient 9, Ingredient 10' },
-        { name: 'Recipe 2', ingredients: 'Ingredient 11, Ingredient 12' }
-      ];
-    } else {
-      this.recipes = [];
-    }
-  }
-}
-
-interface Recipe {
-  name: string;
-  ingredients: string;
-}
+ngOnInit(): void {
+  
+}}
