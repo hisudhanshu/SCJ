2e5e9981d8b0eab5e390fc4dd83c4a77fd419893
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-07-07 14:46:18 +0530

    Sorting Table filter done in material view table

--- src/app/components/material/material-view/material-view.component.ts
+++ src/app/components/material/material-view/material-view.component.ts
@@ -10,6 +10,8 @@ export class MaterialViewComponent implements OnInit {
   materials: any[] | undefined;
   filteredMaterials: any[] | undefined;
   searchQuery: string = '';
+  sortDirection: number[] = [1, 1, 1, 1, 1, 1, 1]; // Sort direction for each column
+  sortColumn: string = ''; // Currently sorted column
 
   constructor(private authService: AuthServicesService) { }
 
@@ -43,5 +45,34 @@ export class MaterialViewComponent implements OnInit {
         material?.name?.toLowerCase().includes(this.searchQuery.toLowerCase())
       );
     }
+  }
+
+  sortTable(column: string) {
+    if (this.sortColumn === column) {
+      // Reverse the sort direction if the same column is clicked again
+      this.sortDirection[this.getHeaderIndex(column)] *= -1;
+    } else {
+      // Reset the sort direction and set the new sorted column
+      this.sortDirection = [1, 1, 1, 1, 1, 1, 1];
+      this.sortColumn = column;
+    }
+
+    this.filteredMaterials?.sort((a, b) => {
+      const index = this.getHeaderIndex(column);
+      const direction = this.sortDirection[index];
+
+      if (a[column] < b[column]) {
+        return -1 * direction;
+      } else if (a[column] > b[column]) {
+        return 1 * direction;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  getHeaderIndex(column: string): number {
+    const headers = ['id', 'name', 'm_code', 'm_type', 'm_cost', 'm_Vendor', 'm_inventory'];
+    return headers.indexOf(column);
   }
 }
