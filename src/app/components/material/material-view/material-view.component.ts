2318384e36f7e93f271032c96236d93a08203176
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-07-09 11:15:53 +0530

    All Screen Dynamically Functonality in done

--- src/app/components/material/material-view/material-view.component.ts
+++ src/app/components/material/material-view/material-view.component.ts
@@ -12,6 +12,7 @@ export class MaterialViewComponent implements OnInit {
   searchQuery: string = '';
   sortDirection: number[] = [1, 1, 1, 1, 1, 1, 1]; // Sort direction for each column
   sortColumn: string = ''; // Currently sorted column
+  selectedMaterial: any;
 
   constructor(private authService: AuthServicesService) { }
 
@@ -61,9 +62,9 @@ export class MaterialViewComponent implements OnInit {
       const index = this.getHeaderIndex(column);
       const direction = this.sortDirection[index];
 
-      if (a[column] < b[column]) {
+      if (a && b && a[column] < b[column]) {
         return -1 * direction;
-      } else if (a[column] > b[column]) {
+      } else if (a && b && a[column] > b[column]) {
         return 1 * direction;
       } else {
         return 0;
@@ -74,5 +75,31 @@ export class MaterialViewComponent implements OnInit {
   getHeaderIndex(column: string): number {
     const headers = ['id', 'name', 'm_code', 'm_type', 'm_cost', 'm_Vendor', 'm_inventory'];
     return headers.indexOf(column);
+  }
+  editMaterial(material: any) {
+    material.isEditing = true;
+  }
+
+  updateMaterial(material: any) {
+    // Perform the update operation for the material, e.g., make an API call
+
+    // After updating, set the isEditing flag to false
+    material.isEditing = false;
+  }
+
+  deleteMaterial(material: any) {
+    // Find the index of the material in the filteredMaterials array
+    const index = this.filteredMaterials?.indexOf(material);
+    
+    if (index !== undefined && index !== -1) {
+      // Remove the material from the filteredMaterials array
+      this.filteredMaterials?.splice(index, 1);
+      
+      // Optionally, you can perform additional logic such as sending an HTTP request to delete the material from the server
+      
+      console.log('Deleted material:', material);
+    } else {
+      console.log('Material not found:', material);
+    }
   }
 }
