b99c13f3fc6179e4aad5968e6e0cad2e1863557e
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-07-10 13:46:56 +0530

    Commit

--- src/app/components/Recipe/recipe-comparison/recipe-comparison.component.ts
+++ src/app/components/Recipe/recipe-comparison/recipe-comparison.component.ts
@@ -1,3 +1,4 @@
+// recipe-comparison.component.ts
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComparisonComponent implements OnInit {
 
+  selectedProduct: string = '';
+  selectedRecipe: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addData(): void {
+    this.selectedProduct = (document.getElementById('name') as HTMLSelectElement).value;
+    this.selectedRecipe = (document.getElementById('Recipe') as HTMLSelectElement).value;
+    localStorage.setItem('selectedProduct', this.selectedProduct);
+    localStorage.setItem('selectedRecipe', this.selectedRecipe);
+    this.showModal();
+  }
+
+  showModal(): void {
+    const modal = document.getElementsByClassName('modal')[0] as HTMLElement;
+    modal.style.display = 'block';
+  }
+
+  saveChanges(): void {
+    // Handle save changes functionality here
+    const modal = document.getElementsByClassName('modal')[0] as HTMLElement;
+    modal.style.display = 'none';
   }
 
 }
