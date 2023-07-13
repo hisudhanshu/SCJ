1669ba16ddb6d447ff7373e70ce4bcd5e63ab9b6
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-07-13 10:11:23 +0530

    Recipe View Table Sorting Acending and Decending

--- src/app/components/Recipe/view-recipe/view-recipe.component.ts
+++ src/app/components/Recipe/view-recipe/view-recipe.component.ts
@@ -11,10 +11,11 @@ export class ViewRecipeComponent implements OnInit {
   selectedProductId: number | null = null;
   recipesData: any[] = [];
   selectedRecipe: any;
-  filteredRecipes: any[] = []; // Array to store filtered recipe data
-  searchKeyword: string = ''; // Variable to store the search keyword
+  filteredRecipes: any[] = [];
+  searchKeyword: string = '';
   recipes: any;
-  
+  isAscending: boolean = true;
+  sortColumn: string = '';
 
   constructor(private authService: AuthServicesService) {}
 
@@ -44,77 +45,72 @@ export class ViewRecipeComponent implements OnInit {
         console.log('Error fetching materials:', error);
       }
     );
-    }    
-    getSelectedProductMaterials(): any[] {
-      if (this.selectedProductId === null) {
-        return [];
-      }
-  
-      return this.recipesData.filter(recipe => recipe.P_Id === this.selectedProductId);
+  }
+
+  getSelectedProductMaterials(): any[] {
+    if (this.selectedProductId === null) {
+      return [];
     }
-  
-    showDetails(recipeData: any) {
-      this.selectedRecipe = recipeData;
-      this.selectedProductId = recipeData.Id;
-    }
-  
+
+    return this.recipesData.filter(recipe => recipe.P_Id === this.selectedProductId);
+  }
+
+  showDetails(recipeData: any) {
+    this.selectedRecipe = recipeData;
+    this.selectedProductId = recipeData.Id;
+  }
+
   searchRecipes(): void {
     // No filtering is required for object data
   }
+
   sortTable(column: string) {
-    // Implement sorting logic based on the provided column
-    // ...
+    if (column === this.sortColumn) {
+      // If the same column is clicked again, reverse the sort order
+      this.isAscending = !this.isAscending;
+    } else {
+      // If a different column is clicked, set it as the new sort column
+      this.sortColumn = column;
+      this.isAscending = true;
+    }
+
+    this.filteredRecipes.sort((a, b) => {
+      const valueA = a[column];
+      const valueB = b[column];
+
+      if (valueA < valueB) {
+        return this.isAscending ? -1 : 1;
+      } else if (valueA > valueB) {
+        return this.isAscending ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
   }
 
-  // Edit recipe function
   editRecipe(recipe: any) {
     recipe.isEditing = true;
   }
 
-  // Update recipe function
   updateRecipe(recipe: any) {
     // Implement the logic to update the recipe
-    // ...
     recipe.isEditing = false;
   }
 
   deleteRecipe(recipe: any) {
-    // Implement the logic to delete the recipe
     const index = this.filteredRecipes.indexOf(recipe);
     if (index !== -1) {
       this.filteredRecipes.splice(index, 1);
-      // You may also want to update the original recipesData array
+
       const originalIndex = this.recipesData.findIndex((r: any) => r.Id === recipe.Id);
       if (originalIndex !== -1) {
         this.recipesData.splice(originalIndex, 1);
       }
-      // Reset the selected recipe if the deleted recipe was selected
+
       if (this.selectedRecipe && this.selectedRecipe.Id === recipe.Id) {
         this.selectedRecipe = null;
         this.selectedProductId = null;
       }
     }
   }
-}
-  
-interface Recipe {
-  Id: number;
-  name: string;
-  category: string;
-  brand: string;
-  customer: string;
-  clienttype: string;
-  materials: Material[];
-}
-
-interface Material {
-  Id: number;
-  productName: string;
-  materialName: string;
-  code: string;
-  type: string;
-  quantity: number;
-  cost: number;
-  vendor: string;
-  stock: number;
 }
