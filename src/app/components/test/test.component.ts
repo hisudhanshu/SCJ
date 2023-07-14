34b333795b0fdc14413221a1eced3fbb6566b30a
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-07-14 16:32:58 +0530

    TEST Components Commit

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -15,60 +15,6 @@ export class TestComponent implements OnInit {
 
   constructor(private authService: AuthServicesService) {}
 
-  ngOnInit(): void {
-    this.authService.getRecipes1().subscribe(
-      (response: any) => {
-        if (response.isSuccess && response.productJson !== null) {
-          this.filteredRecipes = JSON.parse(response.productJson);
-        } else {
-          console.log('API request failed or no data received');
-        }
-      },
-      (error: any) => {
-        console.log('Error fetching recipes:', error);
-      }
-    );
-
-    this.authService.getRecipes().subscribe(
-      (response: any) => {
-        if (response.isSuccess && response.jsonData !== null) {
-          this.recipesData = JSON.parse(response.jsonData);
-        } else {
-          console.log('API request failed or no data received');
-        }
-      },
-      (error: any) => {
-        console.log('Error fetching materials:', error);
-      }
-    );
-    }    
-    showDetails(recipeData: any) {
-      this.selectedRecipe = recipeData;
-    }
-  searchRecipes(): void {
-    // No filtering is required for object data
-  }
-  
-}
-
-interface Recipe {
-  Id: number;
-  name: string;
-  category: string;
-  brand: string;
-  customer: string;
-  clienttype: string;
-  materials: Material[];
-}
-
-interface Material {
-  Id: number;
-  productName: string;
-  materialName: string;
-  code: string;
-  type: string;
-  quantity: number;
-  cost: number;
-  vendor: string;
-  stock: number;
-}
+ ngOnInit(): void {
+   
+ }}
